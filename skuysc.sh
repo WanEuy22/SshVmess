ecaca1009ac4f5a005cdc12ea77d972983d7dbfd
WanEuy22 <111190590+WanEuy22@users.noreply.github.com>
2022-10-17 05:53:31 +0700

    Update skuysc.sh

--- skuysc.sh
+++ skuysc.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+apt update && apt upgrade -y && sleep 2 && sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default.disable_ipv6=1 && apt update && apt install -y bzip2 gzip coreutils screen curl unzip
 apt-get install lolcat -y
 clear
 
