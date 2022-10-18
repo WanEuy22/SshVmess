b26fcbf3ed9ef79499535b45db25eab40663211e
WanEuy22 <111190590+WanEuy22@users.noreply.github.com>
2022-10-19 00:12:12 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,12 +3,14 @@ apt update && apt upgrade -y && sleep 2 && sysctl -w net.ipv6.conf.all.disable_i
 apt-get install lolcat -y
 clear
 
-echo -e " _____ ____  _____ _____" | lolcat
-echo -e "|  ___|  _ \| ____| ____|" | lolcat
-echo -e "| |_  | |_) |  _| |  _|" | lolcat
-echo -e "|  _| |  _ <| |___| |___" | lolcat
-echo -e "|_|   |_| \_\_____|_____|" | lolcat
-echo -e "Script By : Horas Marolop Amsal Siregar" | lolcat
+echo -e "
+ _____                ____
+|  ___| __ ___  ___  / ___|  ___
+| |_ | '__/ _ \/ _ \ \___ \ / __|
+|  _|| | |  __/  __/  ___) | (__
+|_|  |_|  \___|\___| |____/ \___|" | lolcat
+echo -e "--------------------------------------" | lolcat
+echo -e "Script By : Horassss" | lolcat
 echo -e "Recode By : WaanStore" | lolcat
 echo -e "--------------------------------------" | lolcat
 echo -e "××××××××REQUEST ACCESS SCRIPT×××××××××" | lolcat
